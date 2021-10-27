08ad4f7d2e82d0320bc3bc584d1c0be91b66b896
Alexander Clausen <alex@gc-web.de>
2021-10-27 14:31:20 +0200

    Fix testcase

--- tests/test_perf_decorator.py
+++ tests/test_perf_decorator.py
@@ -7,4 +7,4 @@ from perf_utils import perf
 def test_perf_decorator(tmp_path):
     with perf(name="test", output_dir=str(tmp_path)):
         time.sleep(0.5)
-    assert os.path.exists(tmp_path + "test.svg")
+    assert os.path.exists(tmp_path / "test.svg")
